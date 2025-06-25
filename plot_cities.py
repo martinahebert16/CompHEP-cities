a95bdef12e7ea5b9a4b50efe9bf1397b502a8680
Martina Hebert <martinahebert@berkeley.edu>
2025-06-25 11:24:35 -0700

    added features to map

--- plot_cities.py
+++ plot_cities.py
@@ -10,11 +10,16 @@ scale = 5
 map = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49,
         projection='lcc',lat_1=32,lat_2=45,lon_0=-95)
 
+map.shadedrelief()
+
 map.drawstates()
 map.drawcoastlines()
 map.drawmapboundary(fill_color='blue')
 map.fillcontinents(color='yellow', lake_color='blue')
 map.drawcountries()
+map.drawrivers(color='blue')
+map.drawparallels(range(20, 60, 10), labels=[1,0,0,0])
+map.drawmeridians(range(-130, -60, 10), labels=[0,0,0,1])
 
 # Get the location of each city and plot it
 for (city, latitude, longitude) in cities:
